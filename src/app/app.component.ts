f282abace789e1be8ae6c7b43196505bd7cccc30
khaliljedda <khaliljedda4@gmail.com>
2019-12-06 03:19:31 +0100

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import '@clr/icons';
+import '@clr/icons/shapes/essential-shapes';
+import '@clr/icons/shapes/technology-shapes';
+import '@clr/icons/shapes/all-shapes';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'nuit-info';
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { ClarityModule } from "@clr/angular";
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SideNavComponent } from './side-nav/side-nav.component';
+import { CategoryComponent } from './category/category.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SideNavComponent,
+    CategoryComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+        ClarityModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
